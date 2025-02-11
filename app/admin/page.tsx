63fc1a2c3d03f3ce419099a3a480c928e616b7fc
MyanOki-Events <myanmarokinawaevents@gmail.com>
2025-02-11 15:38:55 +0900

    create initial project structure

---
+++ app/admin/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+//import { useSession } from "next-auth/react";
+
+const AdminPage = () => {
+//   const { data: session } = useSession();
+
+//   if (!session || session.user.email !== "admin@example.com") {
+//     return <p>You need to be an admin to view this page.</p>;
+//   }
+
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      {/* Admin functionalities */}
+    </div>
+  );
+};
+
+export default AdminPage;
