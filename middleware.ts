45f43f80df71cdfd80ef49595db58c9c22a52642
MyanOki-Events <myanmarokinawaevents@gmail.com>
2025-02-22 01:45:12 +0900

    add redirecting after login

--- middleware.ts
+++ middleware.ts
@@ -5,18 +5,6 @@ export default withAuth(
     function middleware(req) {
         const token = req.nextauth.token;
 
-        // const sessionToken = req.cookies.get("next-auth.session-token");
-        // const isAuthenticated = !!sessionToken;
-
-        // const url = req.nextUrl.pathname;
-        // console.log("path name", url)
-
-        // if (!isAuthenticated) {
-        //     const loginUrl = new URL("/", req.url);
-        //     loginUrl.searchParams.set("callbackUrl", url);
-        //     return NextResponse.redirect(loginUrl);
-        // }
-        // else 
         if (req.nextUrl.pathname.startsWith("/admin") && token?.role !== 99) {
             return NextResponse.redirect(new URL("/error/not-authorized", req.url));
         }
